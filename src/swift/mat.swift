67aa069f819c93f9b1793c0b63b4b6253eac14d8
David Yu <lewen.yu@gmail.com>
2015-09-15 23:34:36 -0700

    [SWIFT] Mat4

--- src/swift/mat.swift
+++ src/swift/mat.swift
@@ -104,6 +104,98 @@ struct Mat3: Matrix, SpecializedMatrix {
     }
 }
 
+struct Mat4: Matrix, SpecializedMatrix {
+    let rows = 4
+    let cols = 4
+    let count = 16
+    var m = [Float]( count: 16, repeatedValue: 0.0 )
+    subscript( row: Int, col: Int ) -> Float {
+        get {
+            return m[ cols * row + col ]
+        }
+
+        set( newValue ) {
+            m[ cols * row + col ] = newValue
+        }
+    }
+    subscript( i: Int ) -> Float {
+        get {
+            return m[ i ]
+        }
+
+        set( newValue ) {
+            m[ i ] = newValue
+        }
+    }
+    init( m:[Float] ) {
+        self.m = m
+    }
+    var r00: Float {
+        get        { return self[0,0] }
+        set( r00 ) { self[0,0] = r00  }
+    }
+    var r01: Float {
+        get        { return self[0,1] }
+        set( r01 ) { self[0,1] = r01  }
+    }
+    var r02: Float {
+        get        { return self[0,2] }
+        set( r02 ) { self[0,2] = r01  }
+    }
+    var tx: Float {
+        get       { return self[0,3] }
+        set( tx ) { self[0,3] = tx   }
+    }
+    var r10: Float {
+        get        { return self[1,0] }
+        set( r10 ) { self[1,0] = r10  }
+    }
+    var r11: Float {
+        get        { return self[1,1] }
+        set( r11 ) { self[1,1] = r11  }
+    }
+    var r12: Float {
+        get        { return self[1,2] }
+        set( r12 ) { self[1,2] = r12  }
+    }
+    var ty: Float {
+        get       { return self[1,3] }
+        set( ty ) { self[1,3] = ty   }
+    }
+    var r20: Float {
+        get        { return self[2,0] }
+        set( r20 ) { self[2,0] = r20  }
+    }
+    var r21: Float {
+        get        { return self[2,1] }
+        set( r21 ) { self[2,1] = r21  }
+    }
+    var r22: Float {
+        get        { return self[2,2] }
+        set( r22 ) { self[2,2] = r22  }
+    }
+    var tz: Float {
+        get       { return self[2,3] }
+        set( tz ) { self[2,3] = tz   }
+    }
+    var m30: Float {
+        get        { return self[3,0] }
+        set( m30 ) { self[3,0] = m30  }
+    }
+    var m31: Float {
+        get        { return self[7]  }
+        set( m31 ) { self[3,1] = m31 }
+    }
+    var m32: Float {
+        get        { return self[7]  }
+        set( m32 ) { self[3,2] = m32 }
+    }
+    var tw: Float {
+        get       { return self[3,3] }
+        set( tw ) { self[3,3] = tw   }
+    }
+}
+
 func fromRows<R:Nat, C:Nat>( rows: [Vec<C>] ) -> Mat<R, C> {
     var m: [Float] = []
 
@@ -149,6 +241,7 @@ func transpose<R:Nat, C:Nat>( mat: Mat<R, C> ) -> Mat<C, R> {
     return Mat<C,R>( r: mat.rows, c: mat.cols, m: m )
 }
 
+// specialized matrix operations
 func +<T: SpecializedMatrix>( left: T, right: T ) -> T {
     var m = [Float]( count: left.count, repeatedValue: 0.0 )
     for n in 0...left.count - 1 {
