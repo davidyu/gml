ca6160fd8168801e6caa24186ed168e4c92367dc
David Yu <lewen.yu@gmail.com>
2015-10-01 20:12:59 -0700

    [HX] fix base case matrix
    - check matrix dimensions at runtime

--- haxe/gml/matrix/Matrix.hx
+++ haxe/gml/matrix/Matrix.hx
@@ -1,20 +1,44 @@
 package gml.matrix;
 
-import gml.Nat;
-
 // generic matrix
-// TODO make this more matrix-like and less vector-like
-abstract Matrix<T>( Array<T> ) {
-    public function new( xs: Array<T> ) { this = xs; }
+class Matrix<T> {
+    var rows: Int;
+    var cols: Int;
+    var data: Array<T>;
 
-    @:arrayAccess
-    public function get_flat( i: Int ): T {
-        return this[i];
+    public function new( cols: Int, rows: Int, xs: Array<T> ) {
+        this.rows = rows;
+        this.cols = cols;
+        this.data = xs;
+    }
+
+    public function get( r: Int, c: Int ): T {
+        if ( r < this.rows && c < this.cols ) {
+            return this.data[ r * this.cols + c ];
+        } else {
+            throw "given parameters to matrix accessor is out of bounds";
+        }
+    }
+
+    public function set( r: Int, c: Int, v: T ) {
+        if ( r < this.rows && c < this.cols ) {
+            this.data[ r * this.cols + c ] = v;
+        }
     }
 
     @:arrayAccess
-    public function set_flat( i: Int, v: T ): T {
-        this[i] = v;
-        return this[i];
+    public function get_flat( i: Int ): T {
+        if ( i < this.rows * this.cols ) {
+            return this.data[i];
+        } else {
+            throw "given flat index to matrix accessor is out of bounds";
+        }
+    }
+
+    @:arrayAccess
+    public function set_flat( i: Int, v: T ) {
+        if ( i < this.rows * this.cols ) {
+            this.data[i] = v;
+        }
     }
 }
