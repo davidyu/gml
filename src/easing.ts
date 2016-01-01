566d8acc265d086513eb9fa2bc9626ba96826cdd
David Yu <lewen.yu@gmail.com>
2016-01-01 15:46:59 -0800

    Squashed 'ts/' changes from 280d257..2401c90
    2401c90 do not use nonstandard TypedArray functional accessors
    c09d466 hand-roll add and subtract for Mat4 as well
    bece9de bypass get for faster matmul/det/scalarmul
    957a214 hand-roll matmul and scalarmul for Mat4
    7a1730c hand-roll determinant getter for Mat4 to avoid call to Mat.LU
    931da22 oops
    a297f3b hotfix for browsers that don't implement map/reduce for TypedArrays
    f6ed9be oh god
    bd85bf1 fix CircIn, rename suite to TrigIn/Out/InOut
    58ae95d easing eyeball to auto-populate dropdown
    8c7a9e6 add eyeball tests, for easing functions
    f308f0e back in/out ease
    74c26fa fix silly error
    9bc589c exponential easing
    ed89e7e sine-based/circular easing functions
    20288d4 oops
    ec15c0b install tsc and (try to) build everything on Travis
    
    git-subtree-dir: ts
    git-subtree-split: 2401c9048d9a0471d00342eef67d91ac4e5cb565

--- src/easing.ts
+++ src/easing.ts
@@ -1,6 +1,8 @@
 module gml {
   /**
-   * Implements common easing methods (generally used) for animation.
+   * Implements common easing methods (generally used) for tweening/
+   * animations.
+   *
    * All methods assume a normalized input t (time) between 0 and 1
    * and returns an output t' between 0 and 1.
    */
@@ -55,9 +57,9 @@ module gml {
          *
          * we double the input parameter s.t. it is 0 to 1, then pass it into
          * the CubicIn function (t*t*t), then half the result to get an output
-         * from 0 to 0.5. Constant terms cancel to resolve to 4*t*t
+         * from 0 to 0.5. Constant terms cancel to resolve to 4*t*t*t
          */
-        return 4*t*t;
+        return 4*t*t*t;
       } else {
         /* we want verbatim behavior as CubicOut, except we're passing in t
          * with a range of 0.5 to 1, and we want the output to also range from
@@ -69,6 +71,62 @@ module gml {
          */
         let _t = ( ( t - 0.5 ) * 2 ) - 1;
         return ( _t*_t*_t + 1 ) / 2 + 0.5;
+      }
+    }
+
+    public static TrigIn( t: number ) {
+      return 1 - Math.cos( t * ( Math.PI / 2 ) );
+    }
+
+    public static TrigOut( t: number ) {
+      return Math.sin( t * ( Math.PI / 2 ) );
+    }
+
+    public static TrigInOut( t: number ) {
+      if ( t < 0.5 ) {
+        return 0.5 * ( Math.sin( Math.PI * t ) );
+      } else {
+        return -0.5 * ( Math.cos( Math.PI * ( 2 * t - 1 ) / 2 ) - 2 );
+      }
+    }
+
+    public static ExpIn( t: number, base: number = 10 ) {
+      return t == 0 ? 0 : Math.pow( 2, base * ( t - 1 ) );
+    }
+
+    public static ExpOut( t: number, base: number = 10 ) {
+      return t == 1 ? 1 : 1 - Math.pow( 2, -base * t );
+    }
+
+    public static ExpInOut( t: number, base: number = 10 ) {
+      if ( t == 0 ) return 0;
+      else if ( t == 1 ) return 1;
+      else {
+        if ( t < 0.5 ) {
+          return 0.5 * Math.pow( 2, base * ( 2 * t - 1 ) );
+        } else {
+          let _t = ( ( t - 0.5 ) * 2 );
+          return ( 1 - Math.pow( 2, -base * _t ) ) / 2 + 0.5;
+        }
+      }
+    }
+
+    public static BackIn( t: number, s: number = 1.70158 ) {
+      return t * t * ( ( s + 1 ) * t - s );
+    }
+
+    public static BackOut( t: number, s: number = 1.70158 ) {
+      let _t = t - 1;
+      return _t * _t * ( ( s + 1 ) * _t + s ) + 1;
+    }
+
+    public static BackInOut( t: number, s: number = 1.70158 ) {
+      if ( t < 0.5 ) {
+        let _t = t * 2;
+        return 0.5 * _t * _t * ( ( s + 1 ) * _t - s );
+      } else {
+        let _t = ( ( t - 0.5 ) * 2 ) - 1;
+        return ( _t * _t * ( ( s + 1 ) * _t + s ) + 1 ) / 2 + 0.5;
       }
     }
   }
